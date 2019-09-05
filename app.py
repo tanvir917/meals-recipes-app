c3ef2a77a0e8028bbe917ee2c11e43cdda1fe32d
tanvir917 <51207419+tanvir917@users.noreply.github.com>
2019-09-05 21:08:48 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
 @app.route('/tanvir/<int:number>/<int:number1>')
 def default(number,number1):
    try:
